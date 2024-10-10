4cc113d3ce7de8e2b787ea1de400d49a04dca93c
Lakshya <lakshya.nawandhar.457@gmail.com>
2024-10-10 19:41:24 +0530

    db connection

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,12 @@ const {URI}=require('./server.config');
 async  function connecttodb(){
     try {
         // Connect to the MongoDB cluster
-        await mongoose.connect(URI);
+        await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Reduce server selection timeout
+            socketTimeoutMS: 45000,         // Adjust socket timeout
+        });
     console.log("connect to db");
     } catch (e) {
         console.log("not connect to db")
